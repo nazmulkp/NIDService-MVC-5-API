45e9526ac910a21943bb1bce34e93eec94ac50ae
nazmulkp <nazmulkp053@gmail.com>
2015-02-19 00:45:11 -0800

    Models/NidInformation.cs done

---
+++ NIDService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NIDService.WebApiApplication" Language="C#" %>
---
+++ NIDService/Models/NidInformation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NIDService.Models
+{
+    public class NidInformation
+    {
+    }
+}
